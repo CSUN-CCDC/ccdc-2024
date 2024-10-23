64d2727110fe5284fc617b754dd2fec1dfa1495f
moonpiedumplings <jeff.fonseca777@gmail.com>
2024-10-22 19:46:29 -0700

    nix flakey mode, vagrant at 2.3.7

---
+++ devshell.nix
@@ -0,0 +1,41 @@
+{ pkgs ? import <nixpkgs> {}, pkgs-vagrant }:
+
+let
+  my-python-packages = ps: with ps; [
+    ansible
+    ansible-core
+    molecule
+    molecule-plugins
+    dnspython
+    jmespath
+    pywinrm
+    sqlalchemy
+    mysqlclient
+    psycopg2
+    requests
+    pyopenssl
+    pymongo
+    # other python packages
+  ];
+  #vagrantPackage = if builtins.pathExists /etc/wsl.conf then "" else "vagrant";
+  vagrantDefaultProvider = if builtins.pathExists /etc/wsl.conf then "virtualbox" else "libvirt";
+in
+with pkgs;
+mkShell {
+  LC_ALL = "C.UTF-8";
+  LANG = "C.UTF-8";
+  ANSIBLE_HOST_KEY_CHECKING = "False";
+  VAGRANT_WSL_ENABLE_WINDOWS_ACCESS = "1";
+  VAGRANT_DEFAULT_PROVIDER = "${vagrantDefaultProvider}";
+  buildInputs = [
+    podman
+    podman-compose
+    #zellij
+    pkgs-vagrant.legacyPackages.${system}.vagrant
+    openssh
+    sshpass
+    sshs
+    (pkgs.python311.withPackages my-python-packages)
+    ansible-lint
+  ];
+}
--- shell.nix
+++ shell.nix
@@ -1,42 +1,10 @@
-# { pkgs ? import <nixpkgs> { } }:
-{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz) { } }:
-
-let
-  my-python-packages = ps: with ps; [
-    ansible
-    ansible-core
-    molecule
-    molecule-plugins
-    dnspython
-    jmespath
-    pywinrm
-    sqlalchemy
-    mysqlclient
-    psycopg2
-    requests
-    pyopenssl
-    pymongo
-    # other python packages
-  ];
-  #vagrantPackage = if builtins.pathExists /etc/wsl.conf then "" else "vagrant";
-  vagrantDefaultProvider = if builtins.pathExists /etc/wsl.conf then "virtualbox" else "libvirt";
-in
-with pkgs;
-mkShell {
-  LC_ALL = "C.UTF-8";
-  LANG = "C.UTF-8";
-  ANSIBLE_HOST_KEY_CHECKING = "False";
-  VAGRANT_WSL_ENABLE_WINDOWS_ACCESS = "1";
-  VAGRANT_DEFAULT_PROVIDER = "${vagrantDefaultProvider}";
-  buildInputs = [
-    podman
-    podman-compose
-    #zellij
-    vagrant
-    openssh
-    sshpass
-    sshs
-    (pkgs.python311.withPackages my-python-packages)
-    ansible-lint
-  ];
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).shellNix
